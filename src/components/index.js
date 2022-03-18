3da38ecc59de77de4f920d7cd4c1ec3ce4796491
Gjorgjioska <marina.gorgioska@gmail.com>
2022-03-18 19:31:40 +0100

    Footer is displayed

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,6 @@
 export { default as Nabvar } from "./Nabvar";
+export { default as Cryptocurrencies } from "./Cryptocurrencies";
+export { default as CryptoDetails } from "./CryptoDetails ";
+export { default as Exchanges } from "./Exchanges";
+export { default as News } from "./News";
+export { default as Homepage } from "./Homepage";
